b9f99b5b1603afc81346cfc15d695de0c9094188
Ibrahim <ibrahim.benrayana@esprit.tn>
2023-10-03 02:18:51 +0100

    Basic crud Categorie added :white_check_mark:

---
+++ resources/views/Categories/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Cards basic   - UI elements')
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/masonry/masonry.js')}}"></script>
+@endsection
+
+@section('content')
+<div class="container">
+    <h1>Create Category</h1>
+
+    <form method="POST" action="{{ route('categories.store') }}">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Category Name</label>
+            <input type="text" name="name" id="name" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create Category</button>
+    </form>
+</div>
+@endsection
---
+++ resources/views/Categories/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Cards basic   - UI elements')
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/masonry/masonry.js')}}"></script>
+@endsection
+
+@section('content')
+<h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">UI Elements /</span> Cards Basic</h4>
+
+
+@foreach ($categories as $category)
+
+  <div class="col-md">
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img class="card-img card-img-left" src="{{asset('assets/img/elements/12.jpg')}}" alt="Card image" />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{ $category->name }}</h5>
+            <p class="card-text">
+            {{ $category->description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endforeach
+<!--/ Horizontal -->
+@endsection
